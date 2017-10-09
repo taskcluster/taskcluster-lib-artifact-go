fb54ec466d2c871ee9a995a7be1e7a81b7d395a8
John Ford <john@johnford.org>
2017-10-09 17:28:34 +0200

    use Fatal instead of Error for test failures

--- body_test.go
+++ body_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"io/ioutil"
 	"os"
+  "fmt"
 	"testing"
 )
 
@@ -50,19 +51,23 @@ func TestBodyReading(t *testing.T) {
 
 	prepareFiles()
 
+  t.Run("os.IsNotExist(nil)", func(t *testing.T) {
+    fmt.Printf("%+v\n", os.IsNotExist(nil))
+  })
+
 	t.Run("should return error if file doesn't exist", func(t *testing.T) {
 		_, err := newBody("file", 128, 128)
 		if !os.IsNotExist(err) {
-			t.Error(err)
+			t.Fatal(err)
 		}
 	})
 
 	t.Run("should return error if size is zero", func(t *testing.T) {
 		_, err := newBody(filename, 128, 0)
 		if os.IsNotExist(err) {
-			t.Error(err)
+			t.Fatal(err)
 		} else if err == nil {
-			t.Error("Expected an error")
+			t.Fatal("Expected an error")
 		}
 
 	})
@@ -70,22 +75,22 @@ func TestBodyReading(t *testing.T) {
 	t.Run("should read a complete 2048 byte file", func(t *testing.T) {
 		body, err := newBody(filename, 0, 2048)
 		if err != nil {
-			t.Error(err)
+			t.Fatal(err)
 		}
 
 		bodyData, err := ioutil.ReadAll(body)
 		if err != nil {
-			t.Error(err)
+			t.Fatal(err)
 		}
 		body.Close()
 
 		if len(bodyData) != 2048 {
-			t.Error("Body data was not expected 2048 bytes")
+			t.Fatal("Body data was not expected 2048 bytes")
 		}
 
 		for i := 0; i < 2047; i += 8 {
 			if !bytes.Equal(allTheBytes, bodyData[i:i+8]) {
-				t.Errorf("Body data did not match between bytes %d and %d", i, i+8)
+				t.Fatalf("Body data did not match between bytes %d and %d", i, i+8)
 			}
 		}
 	})
@@ -93,22 +98,22 @@ func TestBodyReading(t *testing.T) {
 	t.Run("should read first 1024 bytes of a 2048 byte file", func(t *testing.T) {
 		body, err := newBody(filename, 0, 1024)
 		if err != nil {
-			t.Error(err)
+			t.Fatal(err)
 		}
 
 		bodyData, err := ioutil.ReadAll(body)
 		if err != nil {
-			t.Error(err)
+			t.Fatal(err)
 		}
 		body.Close()
 
 		if len(bodyData) != 1024 {
-			t.Error("Body data was not expected 2048 bytes")
+			t.Fatal("Body data was not expected 2048 bytes")
 		}
 
 		for i := 0; i < 1024; i += 8 {
 			if !bytes.Equal(allTheBytes, bodyData[i:i+8]) {
-				t.Errorf("Body data did not match between bytes %d and %d", i, i+8)
+				t.Fatal("Body data did not match between bytes %d and %d", i, i+8)
 			}
 		}
 	})
@@ -116,22 +121,22 @@ func TestBodyReading(t *testing.T) {
 	t.Run("should read second 1024 bytes of a 2048 byte file", func(t *testing.T) {
 		body, err := newBody(filename, 1024, 1024)
 		if err != nil {
-			t.Error(err)
+			t.Fatal(err)
 		}
 
 		bodyData, err := ioutil.ReadAll(body)
 		if err != nil {
-			t.Error(err)
+			t.Fatal(err)
 		}
 		body.Close()
 
 		if len(bodyData) != 1024 {
-			t.Error("Body data was not expected 2048 bytes")
+			t.Fatal("Body data was not expected 2048 bytes")
 		}
 
 		for i := 0; i < 1024; i += 8 {
 			if !bytes.Equal(allTheBytes, bodyData[i:i+8]) {
-				t.Errorf("Body data did not match between bytes %d and %d", i, i+8)
+				t.Fatalf("Body data did not match between bytes %d and %d", i, i+8)
 			}
 		}
 	})
@@ -139,22 +144,22 @@ func TestBodyReading(t *testing.T) {
 	t.Run("should read middle 1024 bytes of a 2048 byte file", func(t *testing.T) {
 		body, err := newBody(filename, 512, 1024)
 		if err != nil {
-			t.Error(err)
+			t.Fatal(err)
 		}
 
 		bodyData, err := ioutil.ReadAll(body)
 		if err != nil {
-			t.Error(err)
+			t.Fatal(err)
 		}
 		body.Close()
 
 		if len(bodyData) != 1024 {
-			t.Error("Body data was not expected 2048 bytes")
+			t.Fatal("Body data was not expected 2048 bytes")
 		}
 
 		for i := 0; i < 1024; i += 8 {
 			if !bytes.Equal(allTheBytes, bodyData[i:i+8]) {
-				t.Errorf("Body data did not match between bytes %d and %d", i, i+8)
+				t.Fatalf("Body data did not match between bytes %d and %d", i, i+8)
 			}
 		}
 	})
@@ -167,19 +172,19 @@ func TestBodyReading(t *testing.T) {
 		body, err := newBody(filename2, 3, 1)
 		defer body.Close()
 		if err != nil {
-			t.Error(err)
+			t.Fatal(err)
 		}
 		nBytes, err := body.Read(buf)
 		if err != nil {
-			t.Error(err)
+			t.Fatal(err)
 		}
 
 		if nBytes != 1 {
-			t.Errorf("Expected to read a single byte, got %d", nBytes)
+			t.Fatalf("Expected to read a single byte, got %d", nBytes)
 		}
 
 		if buf[0] != 3 {
-			t.Errorf("Expected single byte to be 3, got %d", buf[0])
+			t.Fatalf("Expected single byte to be 3, got %d", buf[0])
 		}
 	})
 }
--- log.go
+++ log.go
@@ -6,9 +6,9 @@ import (
 	"os"
 )
 
-// By default, we're creating a logger which is used to print to the local
-// file.  We are doing to ensure that by default, consumers of this library get
-// useful logs for debugging.  We do, however, want to allow the users to
+// By default, we're creating a logger which is used to print to standard
+// output.  We are doing to ensure that by default, consumers of this library
+// get useful logs for debugging.  We do, however, want to allow the users to
 // change where logging should go, so we'll expose methods to change the
 // behaviour of logging.
 var logger = log.New(os.Stdout, "artifact:", log.Lshortfile|log.LUTC)
